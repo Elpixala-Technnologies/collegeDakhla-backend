025857bcffb460c811b95b031277d4f27480c29f
agam-mishra <agammishra05@gmail.com>
2024-01-29 02:27:05 +0530

    added netlify plugin to project

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+//export default () => ({});
+module.exports = ({ env }) => ({
+	"netlify-deployments": {
+		enabled: true,
+		config: {
+			accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+			sites: [
+				{
+					name: 'sensational-stroopwafel-d341e1',
+					id: "becee51f-86ec-4545-a0f4-a0421824f41d",
+					buildHook: "https://api.netlify.com/build_hooks/65b6bcd52ddcf9a06aabc3e6",
+					branch: 'master' // optional
+				}
+			]
+		},
+	},
+});
