a4614907924ca5b34ac4f482ae0b5571b1a50467
agam-mishra <agammishra05@gmail.com>
2024-02-01 00:44:59 +0530

    adding graphql playground

--- config/plugins.ts
+++ config/plugins.ts
@@ -15,10 +15,11 @@ module.exports = ({ env }) => ({
 		},
 	},
 	graphql: {
+		enabled: true,
 		config: {
 			endpoint: '/graphql',
 			shadowCRUD: true,
-			playgroundAlways: false,
+			playgroundAlways: true,
 			depthLimit: 7,
 			amountLimit: 100,
 			apolloServer: {
