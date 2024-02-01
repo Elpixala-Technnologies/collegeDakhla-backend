506503e7f1a1301c63321954bf39bec3e04229a8
agam-mishra <agammishra05@gmail.com>
2024-02-01 17:34:38 +0530

    prod config for graphql

---
+++ config/env/production/plugins.ts
@@ -0,0 +1,21 @@
+//export default () => ({});
+module.exports = ({ env }) => ({
+	graphql: {
+		enabled: true,
+		config: {
+			endpoint: '/graphql',
+			generateArtifacts: true,
+			artifacts: {
+				schema: true,
+				typegen: true
+			},
+			shadowCRUD: true,
+			playgroundAlways: true,
+			depthLimit: 7,
+			amountLimit: 100,
+			apolloServer: {
+				tracing: false,
+			},
+		},
+	},
+});
