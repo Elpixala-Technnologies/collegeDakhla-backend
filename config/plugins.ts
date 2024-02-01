9b4a28dff106fc23348a2a6afb93b632a78df7ee
agam-mishra <agammishra05@gmail.com>
2024-02-02 00:17:44 +0530

    added import export plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -32,4 +32,16 @@ module.exports = ({ env }) => ({
 			},
 		},
 	},
+	'import-export-entries': {
+		enabled: true,
+		config: {
+			/**
+			 * Public hostname of the server.
+			 *
+			 * If you use the local provider to persist medias,
+			 * `serverPublicHostname` should be set to properly export media urls.
+			 */
+			serverPublicHostname: 'http://localhost:1337', // default: "".
+		},
+	},
 });
